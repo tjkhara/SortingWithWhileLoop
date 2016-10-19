c0dc1259993113eb12c8ae66aaa963a4f85a4e77
Tajeshwar Khara <tkhara@gmail.com>
2016-10-19 18:09:04 +0530

    Sorting an array using while loop
    Sorting with while loop

---
+++ SortingWithWhileLoop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingWithWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortAscendWithWhile();
+        }
+
+        static void SortAscendWithWhile()
+        {
+            int[] arrarySort = new int[] { 100, 23, 45, 42, 17, 23 };
+
+            int holder;
+
+            var j = 0;
+
+            while( j < arrarySort.Length)
+            {
+                int i = 0;
+                while(i < arrarySort.Length - (j+1))
+                {
+                    if (arrarySort[i] > arrarySort[i + 1])
+                    {
+                        holder = arrarySort[i + 1];
+                        arrarySort[i + 1] = arrarySort[i];
+                        arrarySort[i] = holder;
+
+                    }
+                    i++;
+                }
+
+                j++;
+            }
+            
+
+            for(var i = 0; i < arrarySort.Length; i++)
+            {
+                Console.WriteLine(arrarySort[i]);
+            }
+        }
+    }
+}
---
+++ SortingWithWhileLoop/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SortingWithWhileLoop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SortingWithWhileLoop")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64e59124-aac7-4bd6-ab8c-7062552d0dbd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
